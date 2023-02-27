802622782dc85f517156944ce4f8af839ad1989c
anthony-steffen <anthony.steffen@outlook.com.br>
2023-02-27 17:35:11 -0300

    Exemplos - if-else

---
+++ fundamentos/Sc03-Introducao-a-javascript/D01-js-primeiros-passos/script.js
@@ -0,0 +1,9 @@
+let num1 = 8
+let num2 = 8
+if (num1 > num2) {
+    console.log('Vencedor Número 1 ');
+} else if (num1 == num2) {
+    console.log('Empate');
+} else {
+    console.log('Vencedor Número 2 ');
+}
